012429767a56604ed8cc024838d6ef0aee95a9e8
sixtine <sixtine.bs11@gmail.com>
2021-05-27 11:27:17 +0200

    Display all messages

---
+++ frontend/src/components/DisplayMessage.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <ul>
+    <li
+      v-for="item in messages"
+      :key="item.message"
+    >{{ item.message }}</li>
+  </ul>
+</template>
+
+<script>
+const axios = require("axios");
+export default {
+  data() {
+    return {
+      messages: [],
+    };
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/groupomania/message")
+      .then((response) => (this.messages = response.data.message));
+  },
+};
+</script>
+
+<style>
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
   <h1>Bonjour {{ user.name }} !</h1>
   <button v-on:click="disconnection">Déconnexion</button>
-
+  <DisplayMessage />
 </template>
 <script>
 import { mapState } from "vuex";
+import DisplayMessage from "../components/DisplayMessage";
 export default {
+  components: {
+    DisplayMessage,
+  },
   computed: {
     ...mapState(["status", "user", "userInfo"]),
   },
